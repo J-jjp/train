83564f7fc867961bb279cc88ad0559cdc048a133
J-jjp <2053930980@qq.com>
2024-12-09 17:30:47 +0800

    first commit

---
+++ LocomotionWithNP3O-masterV5/LocomotionWithNP3O-master/pdf.py
@@ -0,0 +1,7 @@
+Figure 1: Multiplicity of Behavior (MoB) enables a human to tune a single quadruped policy trained
+on flat ground to diverse unseen environments. Top row: A low-frequency gait fails to sprint on
+slippery terrain (Gait 2; inset) but tuning it to high frequency results in success (Gait 1). However,
+a low frequency and high footswing height are necessary for stair traversal (Gait 2; middle image).
+A low footswing and wide stance (Gait 3) makes the robot robust to leg shoves, but Gait 1, which
+succeeded at sprinting, fails. Tuning gait thus aids in generalizing to different tasks. Bottom row:
+Examples of other behaviors enabled by our controller.
---
+++ LocomotionWithNP3O-masterV5/LocomotionWithNP3O-master/train.py
@@ -0,0 +1,29 @@
+import numpy as np
+import os
+from datetime import datetime
+from configs.go2_constraint_him import Go2ConstraintHimRoughCfg, Go2ConstraintHimRoughCfgPPO
+from configs.go2_constraint_trans_p1 import Go2ConstraintTransP1RoughCfg, Go2ConstraintTransP1RoughCfgPPO
+from configs.go2_constraint_trans_p2 import Go2ConstraintTransP2RoughCfg, Go2ConstraintTransP2RoughCfgPPO
+from configs.tinymal_constraint_him import TinymalConstraintHimRoughCfg, TinymalConstraintHimRoughCfgPPO
+from configs.go1_constraint import Go1ConstraintHimRoughCfg, Go1ConstraintHimRoughCfgPPO
+import isaacgym
+from utils.helpers import get_args
+from envs import LeggedRobot
+from utils.task_registry import task_registry
+# python train.py --task=go2N3poHim
+# python train.py --task=Tinymal
+
+def train(args):
+    env, env_cfg = task_registry.make_env(name=args.task, args=args)
+    ppo_runner, train_cfg = task_registry.make_alg_runner(env=env, name=args.task, args=args)
+    ppo_runner.learn(num_learning_iterations=train_cfg.runner.max_iterations, init_at_random_ep_len=True)
+
+if __name__ == '__main__':
+    task_registry.register("go2N3poHim",LeggedRobot,Go2ConstraintHimRoughCfg(),Go2ConstraintHimRoughCfgPPO())
+    task_registry.register("Tinymal",LeggedRobot,TinymalConstraintHimRoughCfg(),TinymalConstraintHimRoughCfgPPO())
+    task_registry.register("go2N3poTransP1",LeggedRobot,Go2ConstraintTransP1RoughCfg(),Go2ConstraintTransP1RoughCfgPPO())
+    task_registry.register("go2N3poTransP2",LeggedRobot,Go2ConstraintTransP2RoughCfg(),Go2ConstraintTransP2RoughCfgPPO())
+    task_registry.register("go1",LeggedRobot,Go1ConstraintHimRoughCfg(),Go1ConstraintHimRoughCfgPPO())
+    args = get_args()
+    args.task='go1'
+    train(args)
